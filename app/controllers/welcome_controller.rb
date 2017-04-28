a083ef112a9ac78a8153e564cb223d14ac434a6e
Jenny Barnes <jennifer@jennyln.com>
2017-04-27 23:46:59 -0400

    No idea what I'm doing.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,10 @@ class WelcomeController < ApplicationController
 
   # GET /welcome
   def index
-
+    roll_service = RollService.new(params[:uid])
+    roll_service.execute(params[:message])
+    render json: { speech: roll_service.response_message, 
+      displayText: roll_service.response_message  }
   end
 
 end
