daa4399576af9d3065ef939272fe21b891e56a1f
Jenny Barnes <jennifer@jennyln.com>
2017-04-28 09:31:48 -0400

    added totalling

--- app/services/roll_service.rb
+++ app/services/roll_service.rb
@@ -9,10 +9,26 @@ class RollService
   def execute(message)
     if message[:action] == "roll"
         @response_message = roll_dice(message[:parameters][:sides],message[:parameters][:count])
+    elsif message[:action] == "total"
+        @response_message = total_dice(message[:parameters][:sides],message[:parameters][:count],message[:parameters][:mods])
     end
+  end
+  def total_dice(sideses, counts, mods)
+    return_string = ""
+    @total = 0
+    if sideses.respond_to?(:each)
+        roll_hash = sideses.zip(counts).to_h
+        roll_hash.each do |sides, count|
+            return_string += roll_die(sides.to_i,count.to_i)
+        end
+    else
+        return_string += roll_die(sideses.to_i,counts.to_i)
+    end
+    return "The total of all your rolls was #{@total.humanize}. "
   end
   def roll_dice(sideses, counts)
     return_string = ""
+    @total = 0
     if sideses.respond_to?(:each)
         roll_hash = sideses.zip(counts).to_h
         roll_hash.each do |sides, count|
@@ -31,7 +47,9 @@ class RollService
     end
     results = Array.new
     for i in 1..count
-        results << 1 + Random.rand(sides)
+        this_roll = 1 + Random.rand(sides)
+        @total += this_roll
+        results << this_roll
     end
     return_string += results.to_sentence + ". "
     return return_string
