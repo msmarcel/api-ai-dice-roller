da54b92cbc7d8e9bdd12f96d42ae03342545b634
Jenny Barnes <jennifer@jennyln.com>
2017-05-11 16:02:03 -0400

    merged roll and total functions for now

--- app/services/roll_service.rb
+++ app/services/roll_service.rb
@@ -17,37 +17,38 @@ class RollService
     if sideses.respond_to?(:each)
         for i in 0..sideses.count-1
           this_die = counts[i].to_s + "d" + sideses[i].to_s
-          if mods.respond_to?(:each)
-             this_die += mods[i].to_s
-          end
           dice << this_die
         end
     else
         dice << counts.to_s + "d" + sideses.to_s + mods.to_s
     end
-    if !mods.respond_to?(:each) and !mods.to_s.blank?
-      dice << mods.to_s
-    end
     dice_string = dice.join("+")
+    if mods.respond_to?(:each)
+      mods.each do |mod|
+        if !mod.to_s.blank?
+          dice_string += mod.to_s.gsub(/\s+/, "")
+        end
+      end
+    elsif !mods.to_s.blank?
+      dice_string += mods.to_s.gsub(/\s+/, "")
+    end
     #return dice.to_sentence
     @dice_bag = DiceBag::Roll.new(dice_string)
     dice_result = @dice_bag.result()
 
-    if action == "total"
-      return "The total of all your rolls was #{dice_result.to_s.humanize}. "
-    else
-      return roll_to_string(dice_result)
-    end
+    return roll_to_string(dice_result) + "The total of all your rolls was #{dice_result.to_s.humanize}. "
   end
   def roll_to_string(result)
     return_string = ""
     result.each do |section|
-      if section.count > 1
-          return_string = "You rolled #{section.count.humanize} #{section.sides.humanize}-sided dice. Your results were "
-      else
-          return_string = "You rolled a #{section.sides.humanize}-sided die. Your result was "
+      if section.respond_to?(:count) && section.count > 1
+          return_string += "You rolled #{section.count.humanize} #{section.sides.humanize}-sided dice. Your results were "
+      elsif section.respond_to?(:sides)
+          return_string += "You rolled a #{section.sides.humanize}-sided die. Your result was "
       end
-      return_string += section.tally().to_sentence + ". "
+      if (section.respond_to?(:tally))
+        return_string += section.tally().to_sentence + ". "
+      end
     end
     return return_string
   end
